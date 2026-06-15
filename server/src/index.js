a5e5d1b924ce4fa8ee7710a54e171c844fa53cab
yashikagupta-ai <yashikagupta561@gmail.com>
2026-06-15 21:47:41 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -25,6 +25,7 @@ app.use(
         origin.startsWith('http://localhost:') || 
         origin.startsWith('http://127.0.0.1:') ||
         origin.startsWith('https://yashikagupta-ai.github.io');
+        origin.startsWith('https://nayepankh-volunteer-management-plat-one.vercel.app');
 
       if (isAllowed) {
         callback(null, true);
